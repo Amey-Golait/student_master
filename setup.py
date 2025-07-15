b705297e8dade9dca40e487a457ac81dea88770f
Amey Golait <amee@Ameys-MacBook-Air.local>
2025-07-15 17:05:05 +0530

    Add setup.py for packaging and installation

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='student_master',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
