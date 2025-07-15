eb7dc0faeaf2b689170584c59d1ea3e9a91edb08
Amey Golait <amee@Ameys-MacBook-Air.local>
2025-07-15 18:24:46 +0530

    Added Student attendence

---
+++ student_master/student_master/doctype/student_attendance/student_attendance.py
@@ -0,0 +1,35 @@
+import frappe
+from frappe.model.document import Document
+from frappe.utils import nowdate
+
+class StudentAttendance(Document):
+    def validate(self):
+        
+        if not self.academic_year:
+            today = nowdate()
+            academic_year = frappe.db.get_value(
+                "Academic Year",
+                {
+                    "start_date": ["<=", today],
+                    "end_date": [">=", today]
+                },
+                "name"
+            )
+            if academic_year:
+                self.academic_year = academic_year
+            else:
+                frappe.msgprint("No valid Academic Year found for today.")
+
+        existing = frappe.db.exists(
+            "Student Attendance",
+            {
+                "student": self.student,
+                "date": self.date,
+                "name": ["!=", self.name]
+            }
+        )
+
+        if existing:
+            frappe.throw(
+                f"Attendance already marked for {self.student} on {self.date}."
+            )
---
+++ student_master/student_master/doctype/student_attendance/test_student_attendance.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Amey Golait and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestStudentAttendance(FrappeTestCase):
+	pass
